5afd7e0227fb8bdd5bb9a49810978c8110c1367b
Khushi <tandulkarkhushi@gmail.com>
2025-11-14 19:48:33 +0530

    Added evenOdd function

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-function isPrime(num) {
-    if (num <= 1) return false;
-    if (num === 2) return true;
-    if (num % 2 === 0) return false;
 
-    for (let i = 3; i <= Math.sqrt(num); i += 2) {
-        if (num % i === 0) return false;
-    }
-    return true;
+function evenOdd(num) {
+    return num % 2 === 0;
 }
 
-console.log(isPrime(7));
-console.log(isPrime(10));
+console.log(evenOdd(7));
+console.log(evenOdd(4));
