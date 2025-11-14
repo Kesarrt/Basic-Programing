5e7d662e2ffcc26a24421201f8201fee9fa2511e
Khushi <tandulkarkhushi@gmail.com>
2025-11-14 19:49:37 +0530

    Added the prime also

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+function isPrime(num) {
+    if (num <= 1) return false;
+    if (num === 2) return true;
+    if (num % 2 === 0) return false;
+
+    for (let i = 3; i <= Math.sqrt(num); i += 2) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+
+console.log(isPrime(7));
+console.log(isPrime(10));
 
 function evenOdd(num) {
     return num % 2 === 0;
